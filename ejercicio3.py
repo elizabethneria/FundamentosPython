ed1674480a70710640dd03c948fb3fe1af2f667a
elizabethneria <alondraneria88@gmail.com>
2025-05-14 12:50:11 -0600

    ejercicio3

--- ejercicio3.py
+++ ejercicio3.py
@@ -4,10 +4,15 @@
 
 #hipotenusa ** 2 = cateto1 ** 2 + cateto2 ** 2
 
+##el operador doble asterisco ** permite elevar un numero a una potencia
+
+##para operaciones matematicas avanzadas importamos la libreria manth
+
+import math
+
 cateto1 = int(input ('Ingresar valor de Cateto 1: ')) 
 cateto2 = int(input ('Ingresar valor de Cateto 2: ')) 
 
-import math
 hipotenusa = math.sqrt(cateto1 ** 2 + cateto2 ** 2)
 
-print("La hipotenusa del triangulo es:" + str(hipotenusa))
+print("La hipotenusa del triangulo es:" + str(hipotenusa))ejercicio
