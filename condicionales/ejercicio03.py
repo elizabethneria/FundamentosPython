926684c7e9ae986268419f9e93ceea5f62cb0748
elizabethneria <alondraneria88@gmail.com>
2025-05-22 10:44:12 -0600

    ejercicio 3 condicionales

---
+++ condicionales/ejercicio03.py
@@ -0,0 +1,10 @@
+##################################################################################
+## Escribe un programa que lea un número e indique si es par o impar.
+##################################################################################
+
+numero = int(input("Ingresa un numero:"))
+
+if numero % 2 == 0:
+    print("El nuemero es par!")
+else: 
+    print("El nuemero es impar
