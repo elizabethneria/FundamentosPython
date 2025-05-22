40012b1d6ea2a1d7f6bd750de9d9c6c6205072d3
elizabethneria <alondraneria88@gmail.com>
2025-05-22 10:09:12 -0600

    ejercicio 1 condicionales

---
+++ condicionales/ejercicio01.py
@@ -0,0 +1,11 @@
+##################################################################################
+##Algoritmo que pida dos números e indique si el primero es mayor que el segundo.
+##################################################################################
+
+numero1 = int(input("ingresa un numero: "))
+numero2 = int(input("ingresa un numero: "))
+
+if numero1 < numero2:
+    print("El priemer nuemero es mayor")
+
+    
